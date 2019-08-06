fa2f384b7d62f091721373dc35355249cbf9bb4d
Eric Lindahl <eric@sciumo.com>
2019-08-06 10:08:11 -0400

    Correct metadata usage.

--- __tests__/lemfacts.test.ts
+++ __tests__/lemfacts.test.ts
@@ -4,8 +4,10 @@ import {LEMClass,FactBase, LEMField, FactHandle, lemFields}  from "../src/logic_
 @LEMClass
 class TestFact {
   @LEMField name = "name" 
-  constructor(name:string){
+  @LEMField age = 0
+  constructor(name:string, age:number){
     this.name = name;
+    this.age = age;
   }
 }
 
@@ -14,13 +16,15 @@ describe( "LEM", () => {
     test( "LEM has fact handle", () => {
       let myname = "fred";
       let fbase = new FactBase()
-      let w = new TestFact(myname);
+      let w = new TestFact(myname,100);
       expect(w).toBeDefined();
       expect(fbase.inserted).toBeDefined();
       expect(w.name).toEqual(myname);
       let fields = lemFields(w);
       let fhandle = FactHandle.getFor(w);
       expect(fields).toBeDefined();
+      expect(Array.isArray(fields)).toBeTruthy();
+      expect(fields!.length).toEqual(2);
       expect(fhandle).toBeDefined();
     })
   })
