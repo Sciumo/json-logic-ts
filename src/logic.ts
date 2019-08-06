4075c66533e85fb27a8907a112e12942803a06ed
Tom Pollack <tompollack@hotmail.com>
2019-08-06 12:13:54 -0400

    Update Type Equal

--- src/logic.ts
+++ src/logic.ts
@@ -101,7 +101,7 @@ export class Logic implements ILogic {
     }
     const first = values[0];
     for( let i = 1; i < values.length; i++ ){
-      if( first == values[i] ){
+      if( first === values[i] ){
         return false;
       }
     }
